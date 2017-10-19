e446a8aeb38730285b4ce9e6d724df650abeaf0b
Michael Andres <Michael Andres Lozano Sanchez>
2017-10-19 10:50:33 -0500

    Creacion visual de los campos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  base: number = 0;
+  exponente: number = 0;
+  modulo: number = 0;
+  binario: string;
+  arrayBinario: Array<string>;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  converExpToBin(){
+    var numero = parseInt(this.exponente.toString());
+    this.binario = numero.toString(2);
+    this.arrayBinario = this.binario.split("");
   }
 
 }
